a7d69545be35f3f72ccab8a9c8e696cfaba11e25
Gabriel <gabrielwilsonvr@gmail.com>
2026-05-15 12:15:13 -0300

    agr vai

--- lib/types.ts
+++ lib/types.ts
@@ -12,6 +12,7 @@ export interface KnifeProduct {
 
 export interface ProductWithStatus extends KnifeProduct {
   status: StockStatus;
+  alerta: boolean;
   quantidadeSugerida: number;
   valorInvestidoItem: number;
 }
